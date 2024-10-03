239559ba2caf50e6ba018f5f1739e30cedf40ce2
GLinBoy <glinboy@live.com>
2024-10-03 12:46:17 +0200

    👌 IMPROVE: Add Save Book method impl.

--- repository/book_repository_impl.go
+++ repository/book_repository_impl.go
@@ -1,6 +1,7 @@
 package repository
 
 import (
+	"github.com/glinboy/fiber-crud-rest-api-demo/model"
 	"gorm.io/gorm"
 )
 
@@ -10,4 +11,11 @@ type BookRepositoryImpl struct {
 
 func NewBookRepositoryImpl(DB *gorm.DB) BookRepository {
 	return &BookRepositoryImpl{DB: DB}
+}
+
+func (b BookRepositoryImpl) Save(book model.Book) {
+	result := b.DB.Create(&book)
+	if result.Error != nil {
+		panic(result.Error)
+	}
 }

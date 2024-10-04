658ae932959bb356c712fb2d4e663a54f2752ae9
GLinBoy <glinboy@live.com>
2024-10-04 12:05:47 +0200

    📦 NEW: Add Book service API

---
+++ service/book_service.go
@@ -0,0 +1,11 @@
+package service
+
+import "github.com/glinboy/fiber-crud-rest-api-demo/model"
+
+type BookService interface {
+	FindAll() []model.Book
+	FindById() model.Book
+	Save(book model.Book)
+	Update(book model.Book)
+	Delete(id int)
+}

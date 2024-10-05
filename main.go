c58b1c836bc1a1a999415af47de8809133519430
GLinBoy <glinboy@live.com>
2024-10-05 12:56:53 +0200

    👌 IMPROVE: Initiate book repository, service, and router

--- main.go
+++ main.go
@@ -1,14 +1,27 @@
 package main
 
 import (
+	"github.com/glinboy/fiber-crud-rest-api-demo/config"
+	"github.com/glinboy/fiber-crud-rest-api-demo/model"
+	"github.com/glinboy/fiber-crud-rest-api-demo/repository"
 	"github.com/glinboy/fiber-crud-rest-api-demo/router"
+	"github.com/glinboy/fiber-crud-rest-api-demo/service"
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
+
+	db := config.DatabaseConnection()
+
+	db.Table("book").AutoMigrate(&model.Book{})
+
+	bookRepository := repository.NewBookRepositoryImpl(db)
+
+	bookService := service.NewBookServiceImpl(bookRepository)
+
 	app := fiber.New()
 
-	router.NewRouter(app)
+	router.NewRouter(app, bookService)
 
 	app.Listen(":8080")
 }

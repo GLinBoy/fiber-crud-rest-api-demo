f2a591c5f0ed776fc9e54f9da6e5bf1beaa3ac40
GLinBoy <glinboy@live.com>
2024-10-05 11:40:22 +0200

    👌 IMPROVE: Update Fiber app to use router

--- main.go
+++ main.go
@@ -1,15 +1,14 @@
 package main
 
 import (
+	"github.com/glinboy/fiber-crud-rest-api-demo/router"
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
 	app := fiber.New()
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, Fiber!")
-	})
+	router.NewRouter(app)
 
 	app.Listen(":8080")
 }
